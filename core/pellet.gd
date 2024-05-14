0b69fe42b6358c13f26ee57bd698d3b688c77cd7
Devin Price <devin@nowyoujump.com>
2024-05-13 20:01:56 -0700

    pellet burst effect

--- core/pellet.gd
+++ core/pellet.gd
@@ -2,6 +2,8 @@ class_name Pellet extends Area3D
 
 signal consumed
 
+@export var _consume_effect: PackedScene
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
@@ -9,9 +11,16 @@ func _on_body_entered(body: Node3D) -> void:
 	if body is Character:
 		_on_character_entered(body)
 
-func _on_character_entered(character: Character) -> void:
-	print("Character entered!")
+func _on_character_entered(_character: Character) -> void:
 	_consume()
+	if _consume_effect:
+		var fx = _consume_effect.instantiate()
+		get_parent_node_3d().add_child(fx)
+		if fx is VisualInstance3D:
+			fx.global_position = %Collision.global_position
+		if fx is GPUParticles3D:
+			fx.emitting = true
+			fx.finished.connect(queue_free)
 
 func _consume() -> void:
 	consumed.emit()
