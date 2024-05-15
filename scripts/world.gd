350f4f0e7d349ccb0a457f5f0d69705ebd8644db
Devin Price <devin@nowyoujump.com>
2024-05-14 20:44:30 -0700

    update world ready logic

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,17 +2,31 @@ extends Node3D
 
 @export var _levels: Array[Level] = []
 
-var _current_level_index: int = 0
+var _current_level_index: int = -1
 var _current_level: Level:
 	get:
-		if not _levels or _levels.size() == 0: return null
+		if not _levels: return null
+		var size = _levels.size()
+		if size < 1 or _current_level_index < 0 or _current_level_index > size: return null
 		return _levels[_current_level_index]
 
 func _ready():
 	await get_tree().create_timer(2.0).timeout
-	var level = _current_level
-	if level:
-		level.start_level()
+	start_next_level()
 	var ghosts = find_children("*", "Ghost", false)
 	for ghost in ghosts:
 		ghost.mode = Ghost.Mode.CHASE
+
+func start_current_level() -> void:
+	var level = _current_level
+	if level:
+		level.start_level()
+
+func start_next_level() -> void:
+	var level = _current_level
+	if level:
+		level.clear_level()
+	_current_level_index += 1
+	var next_level = _current_level
+	if next_level:
+		next_level.start_level()
