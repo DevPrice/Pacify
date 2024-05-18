3fa286d772187772ef80dac4c5e03908495b8031
Devin Price <devin@nowyoujump.com>
2024-05-17 20:44:53 -0700

    pellet tutorial

--- scripts/tutorial_level.gd
+++ scripts/tutorial_level.gd
@@ -1,10 +1,11 @@
 class_name TutorialLevel extends Level
 
-@export var completion_zone: Area3D
+@export var _completion_zone: Area3D
+@export_file("*.dtl") var _pellet_consume_dialog: String
 
 func _ready():
-	if completion_zone:
-		completion_zone.body_entered.connect(
+	if _completion_zone:
+		_completion_zone.body_entered.connect(
 			func (body: Node3D):
 				if body == _player:
 					level_completed.emit()
@@ -13,6 +14,14 @@ func _ready():
 func _update_player_spawn():
 	if %PlayerSpawn and _player and _player.is_on_floor():
 		%PlayerSpawn.global_position = _player.global_position
+
+func _on_pellet_consumed():
+	if _pellet_consume_dialog and _player:
+		var layout = Dialogic.start(_pellet_consume_dialog)
+		_player.register_character(layout)
+		_player.process_mode = PROCESS_MODE_DISABLED
+		await Dialogic.timeline_ended
+		_player.process_mode = PROCESS_MODE_INHERIT
 
 func _spawn_ghosts():
 	pass
