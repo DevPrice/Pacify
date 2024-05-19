3bc30a6b5f2687b3800c6a1a5066c9d1fb928c84
Devin Price <devin@nowyoujump.com>
2024-05-18 17:20:58 -0700

    full screen toggle

--- ui/settings_menu.gd
+++ ui/settings_menu.gd
@@ -1,5 +1,10 @@
 class_name SettingsMenu extends PanelContainer
 
+func _ready():
+	get_window().size_changed.connect(_refresh)
+	_refresh()
+	%FullscreenButton.pressed.connect(GameInstance.toggle_fullscreen)
+
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		close()
@@ -7,3 +12,7 @@ func _unhandled_input(event):
 
 func close():
 	queue_free()
+
+func _refresh():
+	var window = get_window()
+	%FullscreenButton.button_pressed = window and window.mode == Window.MODE_FULLSCREEN
